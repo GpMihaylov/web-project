71007f95b908d2ef91cce57463b36526245d183e
Georgi Mihaylov <georgimihaylov2002@gmail.com>
2024-06-22 13:18:41 +0300

    fix sql

--- config/createDataBase.sql
+++ config/createDataBase.sql
@@ -1,34 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1
--- Време на генериране:  8 юни 2022 в 17:04
--- Версия на сървъра: 10.4.22-MariaDB
--- Версия на PHP: 8.1.2
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 CREATE DATABASE IF NOT EXISTS `doctrack_3mi0600047_6mi0600059` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `doctrack_3mi0600047_6mi0600059`;
-
---
--- База данни: `doctrack_3mi0600047_6mi0600059`
---
-
--- --------------------------------------------------------
-
---
--- Структура на таблица `uploadeddocument`
---
 
 CREATE TABLE IF NOT EXISTS `uploadeddocument` (
   `file_name` text NOT NULL,
@@ -43,11 +18,6 @@ CREATE TABLE IF NOT EXISTS `uploadeddocument` (
   `upload_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Структура на таблица `user`
---
 
 CREATE TABLE IF NOT EXISTS `user` (
   `email` text NOT NULL,
@@ -56,30 +26,13 @@ CREATE TABLE IF NOT EXISTS `user` (
   `is_admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Схема на данните от таблица `user`
---
 
 INSERT INTO `user` (`email`, `salt`, `password`, `is_admin`) VALUES
 ('admin', '7d702218b95f7e736d7d6034038e998a2b9b4f192e02a655d776a328a051e4fc', '7dcYomqN0tXYY', 1);
 
---
--- Indexes for dumped tables
---
-
---
--- Индекси за таблица `uploadeddocument`
---
 ALTER TABLE `uploadeddocument`
   ADD PRIMARY KEY (`file_name`(200),`user`(100));
 
---
--- Индекси за таблица `user`
---
 ALTER TABLE `user`
   ADD PRIMARY KEY (`email`(100));
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- config/insertData.sql
+++ config/insertData.sql
@@ -1,40 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 09, 2024 at 05:30 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `doctrack_3mi0600047_6mi0600059`
---
 USE `doctrack_3mi0600047_6mi0600059`;
-
--- --------------------------------------------------------
-
---
--- Dumping data for table `user`
---
 
 INSERT INTO `user` (`email`, `salt`, `password`, `is_admin`) VALUES
 ('georgi@gmail.com', '5db26aa8596f41e6e1efbd52dbe13e07a41da2feb27ea3833bcb6c28b82824aa', '5dwOQ6q/0Kw/E', 0);
-
-
---
--- Dumping data for table `uploadeddocument`
---
 
 INSERT INTO `uploadeddocument` (`file_name`, `user`, `location`, `category`, `archived`, `times_downloaded`, `access_key`, `document_priority`, `status`, `upload_date`) VALUES
 ('document1.txt', 'georgi@gmail.com', './upload/66659e5c99cff.txt', 'OtdelStudenti', 0, 0, '9jjxdvovdptlw364w9xh', 'low', 'Нов', '2024-06-21 12:00:00'),
@@ -45,8 +16,4 @@ INSERT INTO `uploadeddocument` (`file_name`, `user`, `location`, `category`, `ar
 ('document6.txt', 'georgi@gmail.com', './upload/6665ca3256e6f.txt', 'NoCategory', 0, 0, 'yy94s4h2d3csew4i4grrg', 'high', 'Нов', '2024-06-21 12:00:00'),
 ('document7.txt', 'georgi@gmail.com', './upload/6665ca7c24ca6.txt', 'OtdelStudenti', 0, 0, 'gx0zxhyadb7sal5p1bek6', 'low', 'Нов', '2024-06-21 12:00:00');
 
-
 COMMIT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
